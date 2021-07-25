cd25619bcdab6cc1296572cc8a00e6ebd1a8bfcd
hassen amri <hassenamri005@gmail.com>
2021-07-25 16:15:29 +0100

    first commit

---
+++ resources/views/footer.blade.php
@@ -0,0 +1,14 @@
+
+<div class="footer card">
+    <div class="card-header">
+      MyShop
+    </div>
+    <div class="card-body">
+      <blockquote class="blockquote mb-0">
+        <p>© 2020-2021 MyShop - Designed and Developped By : <b><a href="#">Hassen Amri</a></b></p>
+        <footer class="blockquote-footer">
+            © 2020-2021 MyShop - Designed and Developed By : <b><a href="#">Hassen Amri</a></b><br>
+            Visit us on :  <cite title="Source Title">Hassen Amri</cite></footer>
+      </blockquote>
+    </div>
+</div>
